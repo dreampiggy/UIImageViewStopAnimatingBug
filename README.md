887c3e9bdedce50cc1e72a82b0e880ca5690aa01
DreamPiggy <lizhuoli1126@126.com>
2024-08-30 15:57:51 +0800

    Updated the README

---
+++ README.md
@@ -0,0 +1,9 @@
+## What's for
+
+TLDR: on iOS 16+, when a `UIImageView` set with `_UIAnimatedImage` (created by using `UIImage animatedImageWithImages:duration`), then `stopAnimating` can not stop the animation at all.
+
+on iOS 15-, everything works fine.
+
+## Demo
+
+Just open `UIImageViewStopAnimatingBug.xcodeproj`, run on devices/simulator, click the Demo's `Stop` button to see the result. Compared with iOS 16+ and iOS 15-
--- UIImageViewStopAnimatingBug/ViewController.m
+++ UIImageViewStopAnimatingBug/ViewController.m
@@ -9,13 +9,53 @@
 
 @interface ViewController ()
 
+@property UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
+- (UIImage *)createUIAnimatedImage {
+    CGSize size = CGSizeMake(100, 100);
+    UIGraphicsImageRenderer *render = [[UIGraphicsImageRenderer alloc] initWithSize:size];
+    UIImage *frame1 = [render imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+        CGContextRef context = rendererContext.CGContext;
+        CGContextSetFillColorWithColor(context, UIColor.redColor.CGColor);
+        CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+    }];
+    UIImage *frame2 = [render imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+        CGContextRef context = rendererContext.CGContext;
+        CGContextSetFillColorWithColor(context, UIColor.blueColor.CGColor);
+        CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+    }];
+    
+    assert(frame1);
+    assert(frame2);
+    
+    UIImage *animatedImage = [UIImage animatedImageWithImages:@[frame1, frame2] duration:1];
+    return animatedImage;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    UIImage *animatedImage = [self createUIAnimatedImage];
+    self.imageView.image = animatedImage;
+    
+    [self.view addSubview:self.imageView];
+    
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(100, 400, 100, 100)];
+    [button setTitle:@"Stop" forState:UIControlStateNormal];
+    [button setBackgroundColor:UIColor.blackColor];
+    [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void)buttonTapped:(UIButton *)sender {
+    [self.imageView stopAnimating]; // BUG!!!! The UIImageView animation can never stop!
+    // You can debug here
 }
 
 
